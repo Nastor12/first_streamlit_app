f6d00b21f398736861a8561dfd59588cd9d4b44d
Nastor12 <119654031+Nastor12@users.noreply.github.com>
2022-12-15 21:06:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title('Hello SnowFlake!')
 st.header('this my first time using streamlit')
 st.text('🍞Hello Bitchs!')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 st.dataframe(my_fruit_list)
 
