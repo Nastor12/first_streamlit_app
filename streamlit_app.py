6ab9f6bbc64d6b33f54dbd732c824719fe5e45aa
Nastor12 <119654031+Nastor12@users.noreply.github.com>
2022-12-16 06:38:43 +0200

    Update streamlit_app.py
    adding multi select

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-
-st.title('Hello SnowFlake!')
-st.header('this my first time using streamlit')
-st.text('🍞Hello Bitchs!')
-
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 st.dataframe(my_fruit_list)
 
