134a90fedb98217079f3b2a5bc2ef3e4e795d567
Nastor12 <119654031+Nastor12@users.noreply.github.com>
2022-12-23 08:12:08 +0200

    removing native response

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,8 @@ st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "Kiwi")
+#st.text(fruityvice_response.json())
 
 normalized_fruit= pd.json_normalize(fruityvice_response.json())
 st.dataframe(normalized_fruit)
