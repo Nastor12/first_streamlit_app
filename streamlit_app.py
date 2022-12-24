b3c5a837bea53c0be58e9e6a3b655570a63fcc9f
Nastor12 <119654031+Nastor12@users.noreply.github.com>
2022-12-24 11:03:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,14 +17,18 @@ st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
 
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+try:
+   fruit_choice = st.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+      st.error('Please select a fruit!')
+   else:
+       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 #st.text(fruityvice_response.json())
+       normalized_fruit= pd.json_normalize(fruityvice_response.json())
+       st.dataframe(normalized_fruit)
+except URLError as e:
+  st.error()
 
-normalized_fruit= pd.json_normalize(fruityvice_response.json())
-st.dataframe(normalized_fruit)
 st.stop()
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
